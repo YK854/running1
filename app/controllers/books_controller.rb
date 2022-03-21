3a7be6be118a8cb5b0c1341748114d5cf118f526
YK854 <95330893+YK854@users.noreply.github.com>
2022-03-21 12:16:33 +0000

    [add]復習

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,45 @@
+class BooksController < ApplicationController
+
+  def index
+    @books = Book.all
+  end
+
+  def new
+    @book = Book.new
+  end
+
+  def create
+    @book = Book.new(book_params)
+    @book.save
+    redirect_to books_path
+  end
+
+  def show
+    @book = Book.find(params[:id])
+  end
+
+  def edit
+    @book = Book.find(params[:id])
+  end
+
+  def update
+    @book = Book.find(params[:id])
+    @book.update(book_params)
+    redirect_to books_path
+  end
+
+  def destroy
+    @book = Book.find(params[:id])
+    if @book.destroy
+      flash[:notice] = '削除に失敗しました'
+      redirect_to books_path
+    end
+  end
+
+  private
+
+  def book_params
+    params.require(:book).permit(:id,:title, :body)
+  end
+
+end
